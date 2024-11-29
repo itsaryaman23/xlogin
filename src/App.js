1b5d52c017984f5d13878fbe99393856fdd991b2
Test <itsaryaman23@gmail.com>
2024-11-29 14:13:18 +0530

    styled version

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(e.target.elements.user.value==="user" && e.target.elements.password.value === "password")
-      {
-        document.getElementsByTagName('form')[0].style.display = "none";
-      document.getElementById('msg').innerText = "Welcome, user!";
-    }
-  else
-  document.getElementById('msg').innerText = "‼️ Invalid username or password ‼️";
-  }
+    if (
+      e.target.elements.user.value === "user" &&
+      e.target.elements.password.value === "password"
+    ) {
+      document.getElementsByTagName("form")[0].style.display = "none";
+      document.getElementById("msg").innerText = "Welcome, user!";
+      document.getElementById('msg').style.color = "white";
+      document.getElementById('msg').style.fontSize = "50px";
+      document.getElementById('msg').style.fontWeight = "500";
+      document.getElementsByClassName('head')[0].style.display = "none";
+    } else
+      document.getElementById("msg").innerText =
+        "‼️ Invalid username or password ‼️";
+  };
   return (
     <div className="App">
-      <h1>Login Page</h1>
-      <p id="msg"><br /></p>
+      <h1 className="head">Login Page</h1>
+      <p id="msg">
+        <br />
+      </p>
       <form onSubmit={handleSubmit}>
-        <label for="username">Username:</label>
-        <input id="username" name="user" required/>
+        {/* <label for="username">Username:</label><br /> */}
+        <input id="username" placeholder="Username" name="user" required />
         <br />
-        <label for="password" >Password:</label>
-        <input id="username" type="password" name="password" required/>
+        {/* <label for="password" >Password:</label><br /> */}
+        <input
+          id="username"
+          placeholder="Password"
+          type="password"
+          name="password"
+          required
+        />
         <br />
-        <button type="submit">Submit</button>
+        <button type="submit" id="subBtn">
+          Submit
+        </button>
       </form>
     </div>
   );
